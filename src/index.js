185115f6320dc958e9c5551b6b930588175f61d7
Danielsuhang <sudan@live.unc.edu>
2019-04-19 13:40:54 -0400

    Initialized obstacles and startpoint using props passed in

--- src/index.js
+++ src/index.js
@@ -2,137 +2,98 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import './index.css';
+class Board extends React.Component {
 
-/* Change Square to be a Function Component */
-class Square extends React.Component {
-      constructor(props) {
-        super(props);
-        this.state = {
-          divStyle: {
-            background: this.props.color
-          }
-        }
-        if (this.props.obstacle) {
-          this.state.divStyle = {background: 'gray'}  
-        }
-        if (this.props.isStart) {
-          this.state.divStyle = {background: 'green'}  
-        } 
-      }
-      render() {
-        return (
-          <button className="square" style={this.state.divStyle} onClick={() => {
-            this.props.onClick();
-            this.setState({
-              divStyle: {background: 'yellow'}
-            })
-          }}>
-          </button>
-        );
-      }
-      
+  /* Lifting State into Parent instead of Child */
+  constructor(props) {
+    super(props);
+    this.state = {
+      rows: this.props.row,
+      columns: this.props.column,
+      squares: this.initBoardData(this.props.row, this.props.column),
+      startPoint: this.props.startPoint,
+      obstacle: this.props.obstacles, 
+    };
+    this.changeColor = this.changeColor.bind(this);
   }
-  
-  class Board extends React.Component {
+  //Initialize Array with Cell Objects
+  initBoardData(row, column) {
+    let data = []
+    for (let i = 0; i < row; i++) {
+      data.push([]);
+      for (let j = 0; j < column; j++) {
+        data[i][j] = {
+          x: i,
+          y: j,
+          curr: false,
+          visited: false,
+          color: 'white'
+        }
+        if (i === this.props.startPoint[0] 
+          && j === this.props.startPoint[1]) {
+            data[i][j].color = 'red';
+        }
+        else if (this.checkObstacle(i,j)) {
+          data[i][j].color = 'black';
+        }
+      }
+    }
+    return data;
+  }
+  changeColor() {
+    let updatedData = this.state.squares.slice();
+    updatedData[5][5].color = "black";
+    this.setState({
+      squares: updatedData
+    })
+  }
+  handleClick(i, j) {
+    /* Create a copy of our squares array to make it immutable */
+    const squares = this.state.squares.slice();
+    this.setState({ squares: squares });
+  }
+  checkObstacle(row, column) {
+    for (let i = 0; i < this.props.obstacles.length; i++) {
+      let x = this.props.obstacles[i][0];
+      let y = this.props.obstacles[i][1];
+      if (row === x && column === y) {
+        return true;
+      }
+    }
+    return false;
+  }
+  renderSquare(i, j) {
+    let divStyle = {
+      background: this.state.squares[i][j].color
+    }
+    return (
+      <button key={i * j + i + j * 2} className="square" style={divStyle}>
+      </button>
+    )
+  }
 
-    /* Lifting State into Parent instead of Child */
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: this.initBoardData(20, 20),
-            startPoint: [15, 5],
-            obstacle: [[5,4], [5,3], [6,6], [6,2]],
-            color: 'white',
-        };
-        this.changeColor = this.changeColor.bind(this);
+  callChildren(row, size) {
+    let children = [];
+    for (let i = 0; i < size; i++) {
+      children.push(this.renderSquare(row, i));
     }
-    //Initialize Array with Cell Objects
-    initBoardData(width, height) {
-      let data = []
-      for (let i = 0; i < width; i++) {
-        data.push([]);
-        for (let j = 0; j < height; j++) {
-          data[i][j] = {
-            x: i,
-            y: j,
-            curr: false,
-            visited: false,
-            color: 'white'
-          }
-        }
-      }
-      return data;
+    return children;
+  }
+  createGrid(size) {
+    let grid = []
+    for (let i = 0; i < size; i++) {
+      grid.push(<div className="board-row" key={i}>{this.callChildren(i, size)}</div>)
     }
-    changeColor() {
-      let updatedData = this.state.squares.slice();
-      updatedData[5][5].color = "black";
-      this.setState({
-        squares: updatedData
-      })
-    }
-    handleClick(i, j) {
-        /* Create a copy of our squares array to make it immutable */ 
-        const squares = this.state.squares.slice();
-        this.setState({squares: squares});
-    }
-    checkObstacle(width, height) {
-      for (let i = 0; i < this.state.obstacle.length; i++) {
-        let x = this.state.obstacle[i][0];
-        let y = this.state.obstacle[i][1];
-        if (width === x && height === y) {
-          return true;
-        }
-      }
-      return false;
-    }
-    renderSquare(i, j) {
-      let obs = false;
-      let startPt = false;
-      if (this.checkObstacle(i,j)) {
-        obs = true;
-      }
-      if (i === this.state.startPoint[0] 
-        && j === this.state.startPoint[1]) {
-          startPt = true;
-        }
-      // return <Square key={i * j + i + j + 2 * i} 
-      // value={this.state.squares[i][j]}
-      // /* Pass in onClick function into Square */
-      // onClick={() => this.handleClick(i, j)} obstacle={obs}
-      // color = {this.state.color}
-      // isStart={startPt}
-      // />;
-      let divStyle = {
-        background: this.state.squares[i][j].color
-      }
-      return (
-        <button key={i*j+i+j*2} className="square" style={divStyle}>
-        </button>
-      )
-    }
-    
-    callChildren(row, size) {
-      let children = [];
-      for (let i = 0; i < size; i++) {
-        children.push(this.renderSquare(row,i));
-      }
-      return children;
-    }
-    createGrid(size) {
-      let grid = []
-      for (let i = 0; i < size; i++) {
-        grid.push(<div className="board-row" key={i}>{this.callChildren(i, size)}</div>)
-      }
-      return grid;
-    }
-    render() {
-      return (
-        <div>
-          <Button varient="contained" color="primary" onClick={this.changeColor}> 
-              Next
-          </Button> 
-          {this.createGrid(this.state.squares.length)}
-          {/* <div className="board-row">
+    return grid;
+  }
+  render() {
+    return (
+      <div>
+        <Button varient="contained" color="primary" onClick={this.changeColor}>
+          Next
+          </Button>
+        {this.createGrid(this.state.squares.length)}
+        {/* <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
@@ -147,32 +108,33 @@ class Square extends React.Component {
             {this.renderSquare(7)}
             {this.renderSquare(8)}
           </div> */}
-        </div>
-      );
-    }
+      </div>
+    );
   }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            
-            <br /> 
-            <Board />
-          </div>
-          <div className="game-info">
-          </div>
-        </div>
-      );
-    }
-  }
+}
 
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
-  
+class Game extends React.Component {
+  render() {
+    let obstacles = [[5, 4], [5, 3], [6, 6], [6, 2]];
+    let startPoint = [15, 5];
+    return (
+      <div className="game">
+        <div className="game-board">
+
+          <br />
+          <Board startPoint={startPoint} obstacles={obstacles} row={20} column={20} />
+        </div>
+        <div className="game-info">
+        </div>
+      </div>
+    );
+  }
+}
+
+
+// ========================================
+
+ReactDOM.render(
+  <Game/>,
+  document.getElementById('root')
+);
